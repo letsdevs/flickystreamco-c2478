9624dc5809b96a995418ba9d05bf3b4011095289
letsdevs <letsdevs@protonmail.com>
2025-08-12 10:41:33 +0530

    Create about.tsx

---
+++ src/pages/about.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>About Cinepapa</h1>
+      <p>This is the about page. You can put anything here!</p>
+    </div>
+  );
+}
